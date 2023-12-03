47bc6254bf91be78a932fc301e9728b32e106525
roosterruler <151454307+roosterruler18@users.noreply.github.com>
2023-12-04 02:48:47 +0700

    add

--- src/components/NavBar/Header.tsx
+++ src/components/NavBar/Header.tsx
@@ -1,20 +1,41 @@
-import React from 'react'
+"use client"
+import React from 'react';
 import Image from 'next/image'
-import Container from '@/components/Container'
+import Container from '@/components/layouts/Container'
+import Link from 'next/link';
+import { DarkButton } from '../forms/DarkButton';
+
+const navBar = [
+    { id: 1, name: 'About', link: '/about' },
+    { id: 2, name: 'Services', link: '/services' },
+    { id: 3, name: 'Portfolio', link: '/portfolio' },
+    { id: 4, name: 'Blog', link: '/blog' },
+]
 
 const Header = () => {
     return (
         <header className='header w-full'>
             <Container>
                 <div className="navBar flex items-center justify-between">
-                    <a href="" className="logo">
+                    <a href="/" className="logo">
                         <Image
-                            src="/logo.jpg"
+                            src="/logo.png"
                             alt=""
-                            width={100}
-                            height={100}
+                            width={300}
+                            height={69}
                         />
                     </a>
+                    <ul className="nav flex items-center gap-16">
+                        {navBar.map((item) => (
+                            <li className="nav-item" key={item.id}>
+                                <Link href={item.link} className='nav-link'>
+                                    {item.name}
+                                </Link>
+                            </li>
+                        ))}
+
+                    </ul>
+                    <DarkButton text={'Contact'} buttonLink={'/contact'} buttonClassName={'btn btn-dark__button'} />
                 </div>
             </Container>
         </header>
---
+++ src/components/forms/DarkButton.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import PropTypes from "prop-types";
+import React from "react";
+
+interface Props {
+    buttonClassName: any;
+    text: string;
+    buttonLink: string;
+}
+
+export const DarkButton = ({ buttonClassName, text = "BUTTON", buttonLink }: Props) => {
+    return (
+        <Link
+            className={`${buttonClassName}`}
+            href={`${buttonLink}`}>
+            {text}
+        </Link>
+    );
+};
+
+DarkButton.propTypes = {
+    text: PropTypes.string,
+    buttonClassName: PropTypes.string.isRequired,
+    buttonLink: PropTypes.string.isRequired,
+};
+
