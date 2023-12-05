3f64d77a187904cd7c68c1d59ec318f1a48e8b89
roosterruler <151454307+roosterruler18@users.noreply.github.com>
2023-12-05 23:36:44 +0700

    add

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { Button } from "./Button";
-export { Img } from "./Img";
-export { Line } from "./Line";
-export { List } from "./List";
-export { PagerIndicator } from "./PagerIndicator";
-export { Text } from "./Text";
