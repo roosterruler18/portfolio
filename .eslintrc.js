9df582f02e62f50558c6262f0a80e291097afe27
bhhoang818 <151454307+roosterruler18@users.noreply.github.com>
2023-12-12 17:01:16 +0700

    add

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-// .eslintrc.js
-module.exports = {
-  root: true,
-  extends: ['next', 'next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
-  rules: {
-    // your custom rules here
-  },
-};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,5 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
-
-module.exports = withMT({
-    content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+module.exports = ({
+    content: ["./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         container: {
             center: true,
